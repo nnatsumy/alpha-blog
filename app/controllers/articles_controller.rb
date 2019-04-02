1b201ff0417fa0114e54adeae5ca219eb0605321
nnatsumy <natsumyiass@gmail.com>
2019-04-02 19:34:41 +0000

    add association between user and articles and add debbuger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,9 @@ class ArticlesController < ApplicationController
        @article = Article.new
    end
    def create
+       #debugger
        @article = Article.new(article_params)
+       #@article.user = User.first
        if @article.save
            #do something
            flash[:notice] = "Article was successfully created"
